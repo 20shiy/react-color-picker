c57bd90f436149afc8eb50065e89384d6c0ab21b
20shiy <shiying6120@gmail.com>
2023-10-27 20:22:17 -0400

    built simple color picker using react-color

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { SketchPicker } from "react-color";
 
 function App() {
+  const [color, setColor] = useState("#fff");
+
+  const handleColorChange = (color) => {
+    setColor(color.hex);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
+      <header className="App-header" style={{ backgroundColor: color }}>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -17,6 +25,7 @@ function App() {
         >
           Learn React
         </a>
+        <SketchPicker color={color} onChangeComplete={handleColorChange} />
       </header>
     </div>
   );
